4bc2e2512247a5dc619b5fb0af2614be84029075
AlaaTaima <alaa.ktaima@gmail.com>
2019-07-17 11:47:42 +0300

    creat elements in dom relates #13

--- public/js/dom.js
+++ public/js/dom.js
@@ -4,43 +4,86 @@ let selector = select => {
 let creat = e => {
     return document.createElement(e);
 };
-let append = (to, e) => {
-    to.appendChild(e);
+let append = (parent, e) => {
+    parent.appendChild(e);
 };
 
-const cityName = selector("#city-name");
-const mainSec = selector("#mainSec");
-const weatherSec = selector("#weather");
-
+const flag = creat('img');
+const name = creat('p');
+const region = creat('p');
+const subRegion = creat('p');
+const currency = creat('p');
 const weatherDate=creat('p');
-weatherDate.id="weatherDate";
-weatherDate.setAttribute("class","weather__date");
-append(weatherSec,weatherDate);
-
 const lastUpdate1 = creat("p");
-lastUpdate1.id = "lastUpdate";
-lastUpdate1.setAttribute("class", "weather__last-update");
-append(weatherSec,lastUpdate1);
-
 const time = creat("span");
-time.id = "time";
-time.setAttribute("class", "weather__time");
-append(lastUpdate1,time);
-
-const temp = creat("p");
-temp.id = "temp";
-temp.class = "weather__temp";
-append(weatherSec, temp);
-
-temp.setAttribute("class", "weather__temp");
-const setuation = creat("p");
-setuation.id = "setuation";
-append(weatherSec, setuation);
-
-setuation.setAttribute("class", "weather__setuation");
 const icon = creat("img");
-icon.id = "icon";
+const setuation = creat("p");
+const temp = creat("p");
+time.setAttribute("class", "weather__time");
+lastUpdate1.setAttribute("class", "weather__last-update");
+weatherDate.setAttribute("class","weather__date");
+temp.setAttribute("class", "weather__temp");
+setuation.setAttribute("class", "weather__setuation");
 icon.setAttribute("class", "weather__icon");
+setuation.id = "setuation";
+weatherDate.id="weatherDate";
+lastUpdate1.id = "lastUpdate";
+time.id = "time";
+temp.id = "temp";
+icon.id = "icon";
+temp.class = "weather__temp";
+let mainSec = selector("#mainSec");
+let weatherSec = selector("#weather");
+append(weatherSec,weatherDate);
+append(weatherSec,lastUpdate1);
+append(lastUpdate1,time);
+append(weatherSec, temp);
+append(weatherSec, setuation);
 append(weatherSec, icon);
+let lastUpdate = selector("#time");
+let tempW = selector("#temp");
+let setuationW = selector("#setuation");
+let iconSetuationW = selector("#icon");
+let dateW = selector("#weatherDate");
+let cityName = selector("#city-name");
+let add = selector('#btnSearch');
+let container = selector(".container");
+let url1 ='http://countryapi.gear.host/v1/Country/getCountries';
 
 
+/* Country API */
+
+add.addEventListener('click', function country (e){
+    e.preventDefault();
+    makeRequest(url1, renderUserInfo);
+    const place = cityName.value.trim();
+    const url2 = `http://api.apixu.com/v1/current.json?key=c0f2ccf7b56740debdb175933191507&q=${place}`;
+    makeRequest(url2, callBack);
+});
+let callBack = respons => {
+    let arr = respons.current.last_updated.split(" ");
+    console.log(dateW)
+   dateW.textContent = arr[0];
+    lastUpdate.textContent = "Last Update: " + arr[1];
+    tempW.textContent = respons.current.temp_c + " C °";
+    setuationW.textContent = respons.current.condition.text;
+    iconSetuationW.setAttribute("src", respons.current.condition.icon);
+  };
+
+function renderUserInfo(result) {
+    result.Response.forEach(element => {
+        if(cityName.value === element.Name){
+            flag.src = element.Flag;
+            name.textContent = `Country name is: ${element.Name}`;
+            region.textContent = `Country region: ${element.Region}`;
+            subRegion.textContent = `Country sub-region: ${element.subRegion}`;
+            currency.textContent = `Country currency: ${element.CurrencySymbol}  ${element.CurrencyName}`;
+            append(container,flag);
+            append(container,name);
+            append(container,region);
+            append(container,subRegion);
+            append(container, currency);
+        }
+    });
+
+};
